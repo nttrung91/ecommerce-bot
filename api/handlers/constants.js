6ce809418d6fe703ea43165e8f6d633e42762833
Trung Nguyen <Trung.Nguyen@walmartlabs.com>
2019-05-23 20:52:20 -0700

    Update order status

--- api/handlers/constants.js
+++ api/handlers/constants.js
@@ -15,9 +15,9 @@ module.exports.SLOT_FULFILLMENT_TYPES = {
 };
 
 module.exports.ORDER_STATUS = {
-  SENT_TO_STORE: 'Enviado a Tienda',
-  PACK_COMPLETE: 'Listo para entregar',
-  ORDER_DELIVERED: 'Entregado',
-  ORDER_CANCELLED: 'Cancelado',
-  SENT_TO_FULFILLMENT: 'Enviado a Tienda'
+  SENT_TO_STORE: 'Your order is on the way',
+  PACK_COMPLETE: 'Your order is ready to deliver',
+  ORDER_DELIVERED: 'Your order is delivered',
+  ORDER_CANCELLED: 'Your order is cancelled',
+  SENT_TO_FULFILLMENT: 'Your order is on the way'
 };
