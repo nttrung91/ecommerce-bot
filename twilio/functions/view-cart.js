d48ca7547174b55582e9286951b7ab13f6b26d81
Trung Nguyen <Trung.Nguyen@walmartlabs.com>
2019-05-24 00:36:01 -0700

    upat

---
+++ twilio/functions/view-cart.js
@@ -0,0 +1,29 @@
+const axios = require('axios');
+
+exports.handler = function(context, event, callback) {
+  axios({
+    method: 'POST',
+    url: 'https://universal-ecommerce-bot.herokuapp.com/api/account/login',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    }
+  }).then(response => {
+    const { products, order, jsessionid } = response.data;
+
+    callback(null, {
+      actions: [
+        {
+          remember: {
+            products,
+            order,
+            jsessionid
+          }
+        },
+        {
+          redirect: 'https://glaucous-lapwing-1943.twil.io/show-cart'
+        }
+      ]
+    });
+  });
+};
